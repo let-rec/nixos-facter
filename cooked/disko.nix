24fa31bfde4204b0b82b4c8b5246a64739a677f5
let-rec <toxtayevhamidulloh6997@gmail.com>
2026-09-06 19:48:33 +0500

    add: disko and update flake

---
+++ cooked/disko.nix
@@ -0,0 +1,32 @@
+{
+  disko.devices ={
+    disk = {
+      main = {
+        device = "/dev/sda1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
--- flake.nix
+++ flake.nix
@@ -2,25 +2,31 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    nixos-facter.url = "github:nix-community/nixos-facter";
+    # nixos-facter.url = "github:nix-community/nixos-facter";
 
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = {self, nixpkgs, nixos-facter, disko, ...}@inputs: {
-    nixosConfigurations.cooked = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        disko.nixosModules.disko
-        ./diskol.nix
-
-        nixos-facter.nixosModules.facter
-        ./hardware-facter.nix
-
-        ./configuration.nix
-      ];
-      specialArgs = { inherit inputs; };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = {self, nixpkgs, disko, ...}: 
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./disko.nix
+
+          # nixos-facter.nixosModules.facter
+          # ./hardware-facter.nix
+          {
+            hardware.facter.reportPath = ./facter.json;
+          }
+
+          ./configuration.nix
+        ];
+        # specialArgs = { inherit inputs; };
+      };
+    };
 }
